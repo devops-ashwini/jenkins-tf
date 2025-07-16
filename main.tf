9d10b8c61b1023e583e5600c447db74e6a0e51b3
devops-ashwini <salgarashwini95@gmail.com>
2025-07-16 21:46:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "vm" {
     ami =  "ami-02c7683e4ca3ebf58"
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     key_name = "private-key"           # key pair name also
     
     tags = {
